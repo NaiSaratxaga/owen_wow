3fe614766b6c7a7649d5f1d3bc3d811eea319254
Naiara Saratxaga <99652446+NaiSaratxaga@users.noreply.github.com>
2022-05-07 18:15:32 +0200

    header styles done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,10 @@
 import '../styles/layout/Header.scss';
-import logo from '../images/logo.jpeg';
+import header from '../images/header.png';
 
 function Header() {
   return (
     <header className='header'>
-      <h1 className='header__title'> Owen Wilson </h1>
-      <img className='header__logo' src={logo} alt='Owen' title='Owen' />
+      <img className='header__logo' src={header} alt='Owen' title='Owen' />
     </header>
   );
 }
