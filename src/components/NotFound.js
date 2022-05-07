4e6080063011cc93cbbd0f1d23b13afa0410a71e
Naiara Saratxaga <99652446+NaiSaratxaga@users.noreply.github.com>
2022-05-07 18:58:41 +0200

    Includes the style for the not found page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,12 +1,16 @@
+import '../styles/layout/NotFound.scss';
+
 import { Link } from 'react-router-dom';
 
 const NotFound = () => {
   return (
-    <section className=''>
-      <p> WoOoOoOWWWWoOOooWwWwwWW La URL no existe! :(</p>
-
-      <Link to='/'>Vuelve a nuestra WoOoOoOWWWWoOOooWwWwwWW inicial</Link>
-    </section>
+    <h3 className='notFound__container'>
+      <p className='notFound__ouch'>Ouuuuuuch!</p>
+      <p>WoOoOoOWWWWoOOooWwWwwWW La URL no existe! :(</p>
+      <Link className='notFound__linkHome' to='/'>
+        Vuelve a la página de inicio
+      </Link>
+    </h3>
   );
 };
 
